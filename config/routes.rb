d96d94da574e5858df56d33c3c898a5110a98ace
Mike Levine <mike.levine@gmail.com>
2016-03-18 19:14:32 -0400

    renaming top ten path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '/pictures/top-ten' => 'pictures#index', :as => 'top-ten', :via => [:get]
   resources :pictures do
     get 'score', :on => :member
     get 'search', :on => :collection
