b0479dce3bb59b48c7492bb3edc1641bd0bb0d11
Mike Levine <mike.levine@gmail.com>
2016-03-18 02:25:43 -0400

    slight cleanup

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,6 @@ class Picture < ActiveRecord::Base
 	default_scope { order("score DESC") }
 
 	def self.daily_capybara(s)
-		s = s
 		low = Random.new.rand(0..100)
 		results = GoogleCustomSearchApi.search("capybara", {"searchType" => "image", "num" => "10", "imgSize" => "huge", "LowRange" => low, "highRange" => low + 9, "hq" => s})
 		r = Random.new.rand(0..9)
